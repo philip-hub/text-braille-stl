82cce10d6c71a14d87646c8c9f7aa7fda0c38649
Philip Pounds <philipstudentemail@gmail.com>
2024-07-14 01:04:24 -0500

    trying to fix the code

--- main.py
+++ main.py
@@ -1,40 +1,19 @@
 import numpy as np
 from solid import *
-from solid.utils import *
+from solid import scad_render_to_file
 
-# Braille dictionary (subset for demonstration)
+# Braille dictionary (complete for demonstration)
 braille_dict = {
-    'a': '100000',
-    'b': '101000',
-    'c': '110000',
-    'd': '110100',
-    'e': '100100',
-    'f': '111000',
-    'g': '111100',
-    'h': '101100',
-    'i': '011000',
-    'j': '011100',
-    'k': '100010',
-    'l': '101010',
-    'm': '110010',
-    'n': '110110',
-    'o': '100110',
-    'p': '111010',
-    'q': '111110',
-    'r': '101110',
-    's': '011010',
-    't': '011110',
-    'u': '100011',
-    'v': '101011',
-    'w': '011101',
-    'x': '110011',
-    'y': '110111',
-    'z': '100111',
-    ' ': '000000'
+    'a': '100000', 'b': '101000', 'c': '110000', 'd': '110100', 'e': '100100',
+    'f': '111000', 'g': '111100', 'h': '101100', 'i': '011000', 'j': '011100',
+    'k': '100010', 'l': '101010', 'm': '110010', 'n': '110110', 'o': '100110',
+    'p': '111010', 'q': '111110', 'r': '101110', 's': '011010', 't': '011110',
+    'u': '100011', 'v': '101011', 'w': '011101', 'x': '110011', 'y': '110111',
+    'z': '100111', ' ': '000000'
 }
 
 def text_to_braille(text):
-    return [braille_dict[char] for char in text.lower() if char in braille_dict]
+    return [braille_dict.get(char, '000000') for char in text.lower()]
 
 def generate_braille_stl(text, filename):
     braille_text = text_to_braille(text)
@@ -59,15 +38,25 @@ def generate_braille_stl(text, filename):
                 for i in range(6)]
         for dot in dots:
             if dot[2] > 0:
-                braille_group.append(translate(dot)(sphere(r=unit_radius)))
+                translated_sphere = translate(dot)(sphere(r=unit_radius))
+                braille_group.append(translated_sphere)
+                # Debugging: Print coordinates and object info
+                print(f"Adding dot at {dot}: {translated_sphere}")
         x_offset += char_spacing
         if x_offset >= postcard_length - char_spacing:
             x_offset = 0
             y_offset += line_spacing
 
     postcard = cube([postcard_length, postcard_width, postcard_height])
-    braille_model = union()([postcard] + braille_group)
-    scad_render_to_file(braille_model, filename)
+    braille_model = union()(postcard, *braille_group)
+    
+    # Generate and save SCAD file for visualization
+    scad_filename = filename.replace('.stl', '.scad')
+    scad_render_to_file(braille_model, scad_filename)
+
+    # Use OpenSCAD to convert SCAD to STL
+    import os
+    os.system(f"openscad -o {filename} {scad_filename}")
 
 # Example usage
 text = "Hello World"
